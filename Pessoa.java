aa4ebedd1304d05f82fb34afacd947bf45c72fdf
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-06 21:30:05 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -29,6 +29,45 @@ public class Pessoa {
         this.mae = pessoa;
     }
 
+    public void removerAncestral() {
+        System.out.println("Digite o nome do ancestral de deseja remover: ");
+        String nomeAncestral = sc.nextLine();
+        removerAncestral(nomeAncestral);
+    }
+
+    public void removerAncestral(String nomeAcestral) {
+        if (this.mae == null) {
+            System.out.println("Não existe ancestral com esse nome");
+            return;
+        }
+        if (this.nome == nomeAcestral) {
+            this.mae = this.mae.mae;
+            return;
+        }
+        this.mae.removerAncestral(nomeAcestral);
+    }
+
+    public void alterarNomeDeAncestral() {
+        System.out.println("Digite o nome do ancestral de deseja alterar: ");
+        String nomeAncestral = sc.nextLine();
+        System.out.println("Digite o novo nome: ");
+        String novoNome = sc.nextLine();
+        alterarNomeDeAncestral(nomeAncestral, novoNome);
+
+    }
+
+    public void alterarNomeDeAncestral(String nomeAncestral, String novoNome) {
+        if (this.mae == null) {
+            System.out.println("Não existe ancestral com esse nome");
+            return;
+        }
+        if (this.nome == nomeAncestral) {
+            this.nome = novoNome;
+            return;
+        }
+        this.mae.alterarNomeDeAncestral(nomeAncestral, novoNome);
+    }
+
     public void adicionarAncestral() {
         System.out.println("Digite o nome do novo ancestral: ");
         String nomeDaMae = sc.nextLine();
@@ -37,14 +76,11 @@ public class Pessoa {
     }
 
     public void imprimeArvoreRecursiva() {
-        System.out.println("Sua Geneologia: ");
         System.out.println(nome);
-
         if (mae == null) {
             System.out.println("Função recursiva!");
             return;
         }
-
         mae.imprimeArvoreRecursiva();
     }
 
